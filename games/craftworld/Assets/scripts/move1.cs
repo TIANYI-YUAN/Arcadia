23734f92db4665da692ef6d27899e15399d21241
TIANYI-YUAN <30408281+TIANYI-YUAN@users.noreply.github.com>
2017-08-01 01:29:18 +1000

    game backup

---
+++ games/craftworld/Assets/scripts/move1.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	int x,y = 0;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			y += 1;
+		}
+			
+
+		if (Input.GetKeyUp (KeyCode.UpArrow)) {
+			y -= 1;
+		}
+
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			y -= 1;
+		}
+
+
+		if (Input.GetKeyUp (KeyCode.DownArrow)) {
+			y += 1;
+		}
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			x -= 1;
+		}
+
+
+		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+			x += 1;
+		}
+
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			x += 1;
+		}
+
+
+		if (Input.GetKeyUp (KeyCode.RightArrow)) {
+			x -= 1;
+		}
+
+
+
+
+
+		if (y == 1) {
+			Vector3 position = this.transform.position;
+			position.y++;
+			this.transform.position = position;
+		} else if (y == -1) {
+			Vector3 position = this.transform.position;
+			position.y--;
+			this.transform.position = position;
+		}
+
+		if (x == 1) {
+			Vector3 position = this.transform.position;
+			position.x=position.x + 1;
+			this.transform.position = position;
+		} else if (x == -1) {
+			Vector3 position = this.transform.position;
+			position.x--;
+			this.transform.position = position;
+		}
+
+
+
+
+	}
+}
---
+++ games/craftworld/Assets/scripts/move2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move2 : MonoBehaviour {
+	Vector2 _target = new Vector2 (100,100);
+	Vector2 _from = new Vector2 (0, 0);
+	float _moveTime = 2;
+	float _timeCount = 0;
+	// Use this for initialization
+
+
+
+
+
+
+
+
+
+	void Start () {
+		
+
+
+
+
+	}
+	void Update () {
+		_timeCount += Time.deltaTime;
+		this.transform.position=Vector2.Lerp(_from,_target,_timeCount/_moveTime);
+
+		}
+	// Use this for initialization
+
+}
